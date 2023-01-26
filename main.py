13f95ae0caa24b19581ade7b0f34a4aacc520d5f
Matheus Freire <santosmatheus99@hotmail.com>
2023-01-25 21:06:15 -0300

    Acresentado metodo GET e POST

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
  # 3. Endpoints-
      #  - localhost/game (GET)
      #  - localhost/game (POST)
-     #  -localhost/game/id (GET)
-     #  -localhost/game/id (PUT)
+     #  -localhost/game/PING (GET)
+     #  -localhost/game/PONG (PUT)
      #  -localhost/game (DELETE)
  # 4. Quais recursos 
  
@@ -20,10 +20,24 @@ game = [
 ]
  
 #Consultar (todos)
-@app.route('/game')
+@app.route('/game',methods=['GET'])
 def obter_game():
     return jsonify(game)
 
+#Consultar (PING)
+@app.route('/game/<int:PING>',methods=['GET'])
+def obter_game_por_PING(PING):
+    for game in game:
+       if game.get('PING') == PING:
+           return jsonify(game)
 
+#Editar
+@app.route('game/<int:PING>',methods=['PUT'])
+def editar_game_por_PING(PING):
+    game_alterado = request.get_json()
+    for indice,game in enumerate(game):
+        if game.get('PING') == PING:
+            game[indice].update(game_alterado)
+            return jsonify(game[indice]) 
 
 app.run(port=5000,host='localhost',debug=True)
